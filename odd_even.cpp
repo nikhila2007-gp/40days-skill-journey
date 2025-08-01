b459d0e4415a6d31cbe961825fd9c3c683a0bbea
nikhila2007-gp <nikhila.nss.jor@gmail.com>
2025-08-01 22:47:30 +0530

    Create odd_even.cpp

---
+++ odd_even.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int number;
+    cout << "Enter number: ";
+    cin >> number;
+    if (number % 2 == 0)
+        cout << number << " is Even" << endl;
+    else
+        cout << number << " is Odd" << endl;
+    return 0;
+}
